f21d5791d13b5c26f80567277aec60b91fa74e44
anirbmuk <anirbmuk@live.in>
2020-10-16 20:20:28 +0530

    Updated to JET 9 and redwood theme

--- src/js/main.js
+++ src/js/main.js
@@ -9,40 +9,56 @@
  * Example of Require.js boostrap javascript
  */
 
-requirejs.config({
-  baseUrl: 'js',
+ // The UserAgent is used to detect IE11. Only IE11 requires ES5.
+ (function () {
+  
+  function _ojIsIE11() {
+    var nAgt = navigator.userAgent;
+    return nAgt.indexOf('MSIE') !== -1 || !!nAgt.match(/Trident.*rv:11./);
+  };
+  var _ojNeedsES5 = _ojIsIE11();
 
-  // Path mappings for the logical module names
-  // Update the main-release-paths.json for release mode when updating the mappings
-  paths:
-  //injector:mainReleasePaths
-  {
-    'knockout': 'libs/knockout/knockout-3.5.0.debug',
-    'jquery': 'libs/jquery/jquery-3.4.1',
-    'jqueryui-amd': 'libs/jquery/jqueryui-amd-1.12.1',
-    'promise': 'libs/es6-promise/es6-promise',
-    'hammerjs': 'libs/hammer/hammer-2.0.8',
-    'ojdnd': 'libs/dnd-polyfill/dnd-polyfill-1.0.1',
-    'ojs': 'libs/oj/v7.2.0/debug',
-    'ojL10n': 'libs/oj/v7.2.0/ojL10n',
-    'ojtranslations': 'libs/oj/v7.2.0/resources',
-    'text': 'libs/require/text',
-    'signals': 'libs/js-signals/signals',
-    'customElements': 'libs/webcomponents/custom-elements.min',
-    'proj4': 'libs/proj4js/dist/proj4-src',
-    'css': 'libs/require-css/css',
-    'touchr': 'libs/touchr/touchr'
-  }
-  //endinjector
-  ,
-  config: {
-    ojL10n: {
-      merge: {
-        'ojtranslations/nls/ojtranslations': 'resources/nls/bundle'
+  requirejs.config(
+    {
+      baseUrl: 'js',
+
+      paths:
+      /* DO NOT MODIFY 
+      ** All paths are dynamicaly generated from the path_mappings.json file.
+      ** Add any new library dependencies in path_mappings.json file
+      */
+      // injector:mainReleasePaths
+      {
+        'knockout': 'libs/knockout/knockout-3.5.1.debug',
+        'jquery': 'libs/jquery/jquery-3.5.1',
+        'jqueryui-amd': 'libs/jquery/jqueryui-amd-1.12.1',
+        'hammerjs': 'libs/hammer/hammer-2.0.8',
+        'ojdnd': 'libs/dnd-polyfill/dnd-polyfill-1.0.2',
+        'ojs': 'libs/oj/v9.1.0/debug' + (_ojNeedsES5 ? '_es5' : ''),
+        'ojL10n': 'libs/oj/v9.1.0/ojL10n',
+        'ojtranslations': 'libs/oj/v9.1.0/resources',
+        'text': 'libs/require/text',
+        'signals': 'libs/js-signals/signals',
+        'customElements': 'libs/webcomponents/custom-elements.min',
+        'proj4': 'libs/proj4js/dist/proj4-src',
+        'css': 'libs/require-css/css',
+        'touchr': 'libs/touchr/touchr',
+        'corejs': 'libs/corejs/shim',
+        'chai': 'libs/chai/chai-4.2.0',
+        'regenerator-runtime': 'libs/regenerator-runtime/runtime'
+      }
+      // endinjector
+      ,
+      config: {
+        ojL10n: {
+          merge: {
+            'ojtranslations/nls/ojtranslations': 'resources/nls/bundle'
+          }
+        }
       }
     }
-  }
-});
+  );
+}());
 
 /**
  * A top-level require call executed by the Application.
